de673092d2f4c0c34b6308481f3877394dc89ee3
hynrytero <129818726+hynrytero@users.noreply.github.com>
2025-02-28 00:00:50 +0800

    asd

--- src/userRoutes/index.js
+++ src/userRoutes/index.js
@@ -9,6 +9,7 @@ const signupRoutes = require('./signupRoutes');
 const forgotPasswordRoutes = require('./forgotPasswordRoutes');
 const profileRoutes = require('./profileRoutes');
 const historyRoutes = require('./historyRoutes');
+const credentialsRoutes = require('./credentialsRoutes');
 
 // Define route mountpoints
 router.use('/auth', authRoutes);
@@ -17,6 +18,6 @@ router.use('/signup', signupRoutes);
 router.use('/forgotpassword', forgotPasswordRoutes);
 router.use('/profile', profileRoutes);
 router.use('/history', historyRoutes);
-
+router.use('/credentials', credentialsRoutes);
 
 module.exports = router;
