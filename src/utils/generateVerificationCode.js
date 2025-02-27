744ab727e8d0faf26132a042b2b932b930f7deea
hynrytero <129818726+hynrytero@users.noreply.github.com>
2025-02-27 20:14:12 +0800

    asd

--- src/utils/generateVerificationCode.js
+++ src/utils/generateVerificationCode.js
@@ -1,6 +1,7 @@
 // src/utils/generateVerificationCode.js
+
 function generateVerificationCode() {
     return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
-module.exports = generateVerificationCode();
+module.exports = generateVerificationCode;
