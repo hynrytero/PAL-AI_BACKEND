537213ac60a2f081bc277ec83a5736f8edde0ac6
hynrytero <129818726+hynrytero@users.noreply.github.com>
2025-02-27 15:05:10 +0800

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 // Import route modules
 const authRoutes = require('./authRoutes');
 const homeRoutes = require('./homeRoutes');
+const signupRoutes = require('./signupRoutes');
 
 // Define route mountpoints
 router.use('/auth', authRoutes);
 router.use('/home', homeRoutes);
+router.use('/signup', signupRoutes);
 
 
 module.exports = router;
